43eeabb4a41641324171ccc30a990bf9c18d954e
grittygerm <alexius.lzc@gmail.com>
2021-12-14 12:28:14 +0800

    task 5 with output from browser

--- src/main/java/ibf2021/HttpClientConnection.java
+++ src/main/java/ibf2021/HttpClientConnection.java
@@ -1,9 +1,6 @@
 package ibf2021;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class HttpClientConnection implements Runnable {
@@ -17,17 +14,35 @@ public class HttpClientConnection implements Runnable {
     @Override
     public void run() {
         try (DataInputStream input = new DataInputStream(socket.getInputStream());
-             DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+             BufferedReader br = new BufferedReader( new InputStreamReader(input))) {
 
-            while (true) {
-                String msg = input.readUTF();
-                if("exit".equals(msg))
-                    break;
-                output.writeUTF(msg);
+            System.out.println("request from browser client:");
+            String line;
+            //reads and prints every line from the request
+            while ( (line = br.readLine()) != null) {
+                System.out.println(line);
             }
+
+
+//            byte[] msg = input.readAllBytes();
+//            String msg = input.readUTF();
+//            System.out.println(msg);
+
+                //sample inputs GET<space>/index.html<space>HTTP/1.1
+
+                //sample output HTTP/1.1<space>200<space>OK\r\n
+                //\r\n
+                //contents in bytes
+
+                //msg.
+
+                //output.writeUTF(msg);
+
             socket.close();
 
         } catch (IOException e) {
+            e.printStackTrace();
             //do something... EOFexception when client exits
         }
     }
